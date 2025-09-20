58d2cbf1b08a3ab725ac08be683e5829cc1020b5
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2025-09-20 15:44:24 +0530

    summarizer uses the new util functions

--- backend/app/graph/nodes/summarizer.py
+++ backend/app/graph/nodes/summarizer.py
@@ -1,10 +1,13 @@
 import os
 from typing_extensions import TypedDict
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage
 from langgraph.runtime import Runtime
 
-from .notes import create_path_to_save_notes
-from .formatter import format_one_doc
+from .utils import (
+    create_path_to_save_notes,
+    cache_intermediate_text,
+    handle_llm_markdown_response,
+)
 from app.services import create_llm_instance
 from app.prompts import SUMMARIZER_SYSTEM_PROMPT
 from app.utils import create_simple_logger
@@ -30,15 +33,15 @@ def save_summary(video_id: str, text: str) -> None:
     logger.info(f"Summary saved at: {file_path}")
 
 
-async def summarizer_node(state: SummarizerState, runtime: Runtime) -> SummarizerState:
+async def summarizer_agent(state: SummarizerState, runtime: Runtime) -> SummarizerState:
     """Generates a summary of the given text using an LLM based on the provided runtime configuration."""
-    file_path = save_summary_path(video_id=runtime.context["video_id"])
-    if os.path.exists(file_path) and not runtime.context.get("refresh_notes", False):
-        logger.info(f"Skipping summarization as summary already saved at: {file_path}")
-        with open(file_path, "r") as file:
-            saved_text = file.read()
-        state["summary"] = saved_text
-        return state
+    saved_summary = cache_intermediate_text(
+        video_id=runtime.context["video_id"],
+        note_type="summary",
+        refresh_notes=runtime.context.get("refresh_notes", False),
+    )
+    if saved_summary:
+        return {"summary": saved_summary}
 
     llm = create_llm_instance(
         provider=runtime.context["provider"], model=runtime.context["model"]
@@ -46,16 +49,7 @@ async def summarizer_node(state: SummarizerState, runtime: Runtime) -> Summarize
     system_message = SystemMessage(content=SUMMARIZER_SYSTEM_PROMPT)
     human_message = HumanMessage(content=state["collected_notes"])
     response = await llm.ainvoke([system_message, human_message])
-    if isinstance(response, AIMessage):
-        raw_summary = response.content
-        logger.info("Raw summary generated, proceeding to format it.")
-        formatted_summary = await format_one_doc(
-            llm, raw_summary, current_chunk="summary", runtime=runtime
-        )
-        formatted_summary = formatted_summary["formatted_text"]
-        save_summary(video_id=runtime.context["video_id"], text=formatted_summary)
-        state["summary"] = formatted_summary
-    else:
-        logger.error("Unexpected response type from LLM")
-        state["summary"] = state["collected_notes"]
-    return state
+
+    summary = handle_llm_markdown_response(response)
+    save_summary(video_id=runtime.context["video_id"], text=summary)
+    return {"summary": summary}
